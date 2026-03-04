01181d4fec7d97a375c9a17679b5d6bb45a76968
Adithya Manivannan <adithya05m@gmail.com>
2026-03-04 10:07:51 -0600

    Fix db.create_all for production

--- app.py
+++ app.py
@@ -994,6 +994,9 @@ def winner(actor_id):
 #  RUN
 # ─────────────────────────────────────────────────────
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
